2008f8596a3f0d44fcf6b6105123111166edc2d5
tastyworm <github@tastyworm.com>
2017-03-06 14:28:33 +1100

    Included Node Exception
    Added the node exception

--- Node.java
+++ Node.java
@@ -1,6 +1,6 @@
 
 public class Node {
-	
+
 	private int x;
 	private int y;
 	private int height;
@@ -11,15 +11,26 @@ public class Node {
 	private int out_conny;
 	private String nodeType;
 
-public Node(int x, int y, int height, int width, String nodeType) {
+	public Node(int x, int y, int height, int width, String nodeType) throws NodeTypeException {
 		super();
+
+		boolean wasFound=false;
+		String[] validNodeTypes = new String[] {"I", "O", "H"};
+		for (int i=0; i<validNodeTypes.length; i++){           
+			if (nodeType==validNodeTypes[i]){
+				wasFound=true;
+			}
+	    }
+		
+		if (!wasFound) throw new NodeTypeException("Invalid Node Type: " + nodeType);
+		
 		this.x = x;
 		this.y = y;
 		this.height = height;
 		this.width = width;
-		this.inc_connx = x-1;
-		this.inc_conny = y+((int)(0.5*height));
-		this.out_connx = x+width-1;
+		this.inc_connx = x - 1;
+		this.inc_conny = y + ((int) (0.5 * height));
+		this.out_connx = x + width - 1;
 		this.out_conny = this.inc_conny;
 		this.nodeType = nodeType;
 	}
@@ -48,7 +59,7 @@ public Node(int x, int y, int height, int width, String nodeType) {
 		return inc_conny;
 	}
 
-public int getOut_connx() {
+	public int getOut_connx() {
 		return out_connx;
 	}
 
